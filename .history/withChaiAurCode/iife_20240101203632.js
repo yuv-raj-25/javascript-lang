1af970df55652de41886bc9fa5fe5a2f49354014
yuv-raj-25 <yuvrajchaudhary464@gmail.com>
2024-01-01 20:38:39 +0530

    new changes

---
+++ .history/withChaiAurCode/iife_20240101203632.js
@@ -0,0 +1,16 @@
+//   immediate Invoked Function Expression -> IIFE
+
+// function chai (){
+//     console.log("DB Connected");
+// }
+// chai()
+
+(function chai (){
+    // this is a name IIFE
+    console.log(`DB Connected `);
+})();
+
+((name) =>{
+    console.log(`DB Connected two ${name} `);
+})("yuvraj")
+`
